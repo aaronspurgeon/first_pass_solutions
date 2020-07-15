45ce58869c6d68f1c159235d15f2e879f3c4a56c
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-14 20:10:32 -0400

    Completed single number by using a hash table or dictionary in python

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,25 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    # return the integer in the list that doesn't have a duplicate
+    # an optimized solution here would be to use a hashmap but idk how to do that
+    # i can try a brute force way by looping through the array to create a list of 1 of each
+    # then loop through the array again but this time make a nested for loop of the list i created and keep count of each time
+    my_dict = {}
+    for i in arr:
+        if len(my_dict) == 0:
+            my_dict[i] = 1
+        elif i not in my_dict.keys():
+            my_dict[i] = 1
+        else:
+            my_dict[i] += 1
+    for i in my_dict:
+        if my_dict[i] == 1:
+            return i
 
 
 if __name__ == '__main__':
